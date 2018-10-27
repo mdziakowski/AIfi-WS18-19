fbf69eb1d8b253cd3b5d967e4479917e5e501610
friedrich <friedrich@friedrichs-mbp>
2018-10-27 21:06:22 +0200

    1. HA

---
+++ src/Aufgabenblätter/Blatt_1.java
@@ -0,0 +1,23 @@
+package Aufgabenblätter;
+
+public class Blatt_1 {
+
+	public static void main(String[] args) {
+
+		double a = 365;
+		double x_0 = a / 2;
+		double x_1 = (x_0 + (a / x_0)) / (2);
+
+		while (Math.abs(x_0 - x_1) > 0.0001) {
+
+			x_0 = x_1;
+
+			x_1 = (x_0 + (a / x_0)) / (2);
+
+		}
+
+		System.out.println("Die Wurzel von " + a + " ist ungefähr: " + x_1);
+
+	}
+
+}
