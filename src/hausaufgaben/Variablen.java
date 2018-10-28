e4e4fedd9e6a4ce8c0018bfae535e86706050406
Carmo Kluenker <Carmo Kluenker@192.168.0.15>
2018-10-28 16:46:57 +0100

    Abgabe der ersten freiwilligen Hausaufgabe. Quadratwurzel

---
+++ src/hausaufgaben/Variablen.java
@@ -0,0 +1,18 @@
+package hausaufgaben;
+
+public class Variablen {
+	public static void main (String[] args) {
+	
+		double a=9;							//von a wird die Quadratwurzel bestimmt
+		double xAlt = a/2;					//initialer Lösungsvorschlag
+		
+		double xNeu = (xAlt + (a/xAlt))/2;	//Berechnung von x_1
+		System.out.println(xNeu);
+		
+		do {								// Berechnung und Ausgabe von x_i
+			xAlt=xNeu;
+			xNeu = (xAlt + (a/xAlt))/2;
+			System.out.println(xNeu);
+		}while (xAlt-xNeu>0.001);			// 2 aufeinanderfolgende Lsg unterscheiden sich um weniger als 0.0001
+	}
+}
