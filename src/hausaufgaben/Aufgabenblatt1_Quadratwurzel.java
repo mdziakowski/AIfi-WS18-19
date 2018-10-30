8f7bdbec11c8ca0f950fcadacf65712ac6390957
u434590 <u434590@R-106.cadlab.tu-berlin.de>
2018-10-30 14:08:50 +0100

    Hausaufgabe 1 fertig zur Kontrolle

---
+++ src/hausaufgaben/Aufgabenblatt1_Quadratwurzel.java
@@ -0,0 +1,23 @@
+package einfach;
+
+/**
+ *
+ * @author Pascal Meissner
+ */
+public class Aufgabenblatt1_Quadratwurzel {
+    public static void main (String args[]){
+        final double a = 30;
+        double x_0=a/2;
+        double x_neu=x_0-0.0002;
+        System.out.println("x_0: " + x_0);
+                
+        for (int x_i=2;x_0-x_neu>0.0001;x_i++){
+            x_0=x_neu;
+            x_neu=(x_0+a/x_0)/2;
+            System.out.println("x_" + x_i + " :" + x_neu);
+        }
+    }    
+    
+    
+    
+}
