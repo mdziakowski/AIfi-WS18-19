f25aa56573693060c28795c08f94d1bc35b144e3
Marcel <Marcel@Yoga>
2018-10-30 14:25:43 +0100

    Aufgabe 1

--- src/uebung2/FakultaetFor.java
+++ src/uebung2/FakultaetFor.java
@@ -3,13 +3,10 @@ package uebung2;
 public class FakultaetFor {
 
 	public static void main(String[] args) {
-		int no = 5;
-		long fac = 1;
+		int zahl = 5;
+		long fak = 1;
 		
-		for (int i=1; i<=no; i++) {
-			fac = i*fac;
-		}
-		System.out.println(fac);
+		FakultaetMethode.berechne(zahl, fak);
 	}
 
 }
--- src/uebung2/FakultaetMethode.java
+++ src/uebung2/FakultaetMethode.java
@@ -1,5 +1,12 @@
 package uebung2;
 
 public class FakultaetMethode {
+
+	static void berechne(int zahl, long fak) {
+		for (int i=1; i<=zahl; i++) {
+			fak = i*fak;
+		}
+		System.out.println(fak);
+	}
 // test
 }
