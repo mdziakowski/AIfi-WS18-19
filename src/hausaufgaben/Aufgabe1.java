7e45ddd6dbd7c333207b8d4cb70091d571d03028
berni <berni@wlan-141-23-151-214.tubit.tu-berlin.de>
2018-10-30 15:32:20 +0100

    Methode zur Approximation der Wurzel extrahiert

--- src/hausaufgaben/Aufgabe1.java
+++ src/hausaufgaben/Aufgabe1.java
@@ -3,24 +3,32 @@ package hausaufgaben;
 public class Aufgabe1 {
 	public static void main(String[] args) {
 		
-		double a = 277;
+		double a = 100;
 		double x0 = a/2;
 		double x1 = a;
+		double sqrt_a;
 		int i = 1; 
 		
+		sqrt_a = mySqrt(a, x0, x1, i);
+		
+		System.out.println("Die Wurzel von a = " + a + " ist ungefähr " + sqrt_a);
+		System.out.println("Test mit java Methode Math.sqrt(a) = " + Math.sqrt(a));
+
+	}
+
+	private static double mySqrt(double a, double x0, double x1, int i) {
 		do{ 
 			if (i > 1) {
 				x0 = x1;
 			}
             x1 = (x0 + a/x0)/2; 
-            System.out.println(x1);
+            // System.out.println(x1);
             i++;
            
         }while(Math.abs(x1-x0) > 0.0001); 
 		
-		System.out.println("Die Wurzel von a = " + a + " ist ungefähr " + x1);
-		System.out.println("Test mit java Methode Math.sqrt(a) = " + Math.sqrt(a));
-
+		return x1;
+	
 	}
 
 }
