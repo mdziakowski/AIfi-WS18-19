d3b6bf801300c1669c6cd7dd5858959671a58e4a
u434590 <u434590@R-106.cadlab.tu-berlin.de>
2018-10-30 14:10:52 +0100

    Lotto.java geändert

---
+++ src/uebung2/Lotto.java
@@ -0,0 +1,18 @@
+package uebung2;
+
+public class Lotto {
+
+	public static void main(String[] args) {
+		
+		System.out.println();
+
+	}
+	
+	public static double fak (int zahl_fak){
+		int ausgabe_fak=1;
+		for (int i=1;i<=zahl_fak;i++){
+		ausgabe_fak=ausgabe_fak*i;			
+		}
+		return ausgabe_fak;
+	}
+}
