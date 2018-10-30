7d99ad10b6851f6d41881748690c6c7d88d38c3d
u434590 <u434590@R-106.cadlab.tu-berlin.de>
2018-10-30 14:24:08 +0100

    Update Lotto

--- src/uebung2/Lotto.java
+++ src/uebung2/Lotto.java
@@ -3,16 +3,24 @@ package uebung2;
 public class Lotto {
 
 	public static void main(String[] args) {
+		int Kugeln = 49;
+		int Zuege = 6;
 		
-		System.out.println();
+		System.out.println(binom(Kugeln,Zuege));
 
 	}
 	
 	public static double fak (int zahl_fak){
-		int ausgabe_fak=1;
+		double ausgabe_fak=1;
 		for (int i=1;i<=zahl_fak;i++){
 		ausgabe_fak=ausgabe_fak*i;			
 		}
 		return ausgabe_fak;
 	}
-}
+	
+	public static double binom (int n, int k){
+			double nuk;
+			nuk = fak(n)/(fak(k)*fak(n-k));		
+		return nuk;
+	}
+	}
