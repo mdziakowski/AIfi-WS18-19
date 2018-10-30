24afe92e7e7f699487b2c02c9bdc840a69156e30
berni <berni@wlan-141-23-151-214.tubit.tu-berlin.de>
2018-10-30 15:16:50 +0100

    Aufgabe 3 aus 2. Übung (Bernhard Gülzow - 337059)

--- src/uebung2/Aufgabe2.java
+++ src/uebung2/Aufgabe2.java
@@ -1,10 +1,17 @@
 package uebung2;
 
-public class CalWurzel {
+public class Aufgabe2 {
 	
 	public static void main(String[] args) {
 		
-		int ges = 2;
+		int ges = 3; int out;
+		out = intSqrt(ges);
+		
+		System.out.println("Die letzte natürliche Zahl ist " + out);
+			
+	}
+
+	private static int intSqrt(int ges) {
 		int i = 1;
 		int abbr = 0;
 		double iSquare = 0;
@@ -20,11 +27,13 @@ public class CalWurzel {
 		
 		if (abbr == 1) {
 			System.out.println(ges + " hat keine natürliche Wurzel");
+			
 		}
 		else {
 			System.out.println("Die Wurzel von " + ges + " ist " + (i-1));
 		}
-			
+		
+		return i-1;
 	}
 
 }
