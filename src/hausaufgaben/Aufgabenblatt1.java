b4aa1fecdb81b7b0c1a7209ffa9f0939fd5466bf
denisameltchenko <denisameltchenko@192.168.1.6>
2018-10-31 12:22:58 +0100

    Hausaufgabe 1

--- src/hausaufgaben/Aufgabenblatt1.java
+++ src/hausaufgaben/Aufgabenblatt1.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 	double x0 = a/2;
 	double x1 = (x0+a/x0)/2;
 	
-	while((x0 - x1) > 0.000001) {
+	while((x0 - x1) > 0.0001) {
 		x0 = x1;
 	    x1 = (x0+a/x0)/2;
 		System.out.println(x1);
