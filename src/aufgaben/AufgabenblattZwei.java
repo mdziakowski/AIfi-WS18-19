4fee04f2cd30a62d4767fac26aeefebaf5063018
Nils <n.holtkoetter@outlook.com>
2018-11-06 17:36:35 +0100

    test

---
+++ src/aufgaben/AufgabenblattZwei.java
@@ -0,0 +1,34 @@
+package aufgaben;
+
+public class AufgabenblattZwei {
+
+	public static void main(String[] args) {
+
+	
+		//1
+		public class Rational
+		{
+			int zähler;
+			int nenner;
+			
+			
+			void Konstruktor
+			{
+			zähler = 0;
+			nenner = 1;
+				
+			}
+			public void Correct()
+			{
+				}
+			
+			
+			
+			
+			}
+	
+	
+		
+	}
+
+}
