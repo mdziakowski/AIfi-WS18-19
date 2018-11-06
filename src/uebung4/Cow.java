617b7750a8226ac0cab4bbb02df2ebc446fb7022
Marc <dziakowski@vsp.tu-berlin.de>
2018-11-06 17:34:44 +0100

    uebung 4

--- src/uebung4/Cow.java
+++ src/uebung4/Cow.java
@@ -18,5 +18,17 @@ public class Cow {
 	public static int getCowCount() {
 		return cowCounter;
 	}
-
+	
+	public boolean isHungry() {
+		return this.hungry;
+	}
+	
+	public void toggleHungry() {
+		this.hungry = !this.hungry;
+	}
+	
+	public Calf giveBrith(String name) {
+		return new Calf(name, this);
+	}
+	
 }
