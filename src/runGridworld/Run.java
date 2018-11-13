6c6300e11e08eb03efaf553d05e6bc527e379c89
Marcel <Marcel@Yoga>
2018-11-13 17:11:35 +0100

    Unrelevant

---
+++ src/runGridworld/Run.java
@@ -0,0 +1,14 @@
+package runGridworld;
+
+import gridworld.framework.actor.ActorWorld;
+
+public class Run {
+	
+	public static void main(String[] args) {
+		
+		ActorWorld world = new ActorWorld();
+		
+		world.show();
+	}
+
+}
