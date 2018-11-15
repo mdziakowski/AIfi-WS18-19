238abd96e4418d47f87b4404bb509c5aeede9f68
Friedrich <Friedrich@DESKTOP-3R0P3O2.localdomain>
2018-11-15 20:04:14 +0400

    a

--- src/uebung5/Breeder.java
+++ src/uebung5/Breeder.java
@@ -2,11 +2,24 @@ package uebung5;
 
 import java.awt.Color;
 
-public class Breeder extends DBug {
+import gridworld.framework.actor.Actor;
+import gridworld.framework.actor.Flower;
+import gridworld.framework.grid.Grid;
+import gridworld.framework.grid.Location;
+
+public class Breeder extends Bug {
 	
-	Breeder(Color colorbug) {
-		setColor(colorbug);
+	public Breeder() {
+		this.setColor(Color.RED);
 	}
+	
+	/* @Override
+    public void add(Actor occupant)
+    {
+        Location loc = getRandomEmptyLocation();
+        if (loc != null)
+            add(loc, occupant);
+    } */
 
 
 	
