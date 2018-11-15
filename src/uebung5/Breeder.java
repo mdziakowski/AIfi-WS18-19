f74cce734f24bb62d4b5bdb0170edfe696d1db5d
friedrich <friedrich@friedrichs-mbp>
2018-11-15 16:29:02 +0100

    test

---
+++ src/uebung5/Breeder.java
@@ -0,0 +1,27 @@
+package uebung5;
+
+import java.awt.Color;
+
+public class Breeder extends DBug {
+
+	Breeder(Color colorbug) {
+		super(colorbug);
+		// TODO Auto-generated constructor stub
+	}
+
+	/* Breeder(Color red) {
+		
+		setColor(Color.RED);
+	
+	}
+	
+	Breeder() {
+	
+		setColor(Color.RED);
+	
+	} */
+	
+	
+	
+
+}
