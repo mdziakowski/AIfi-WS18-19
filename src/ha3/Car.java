dbf9611cb809f037aecc51725b2b02329004ab14
Beulenbernd <maximilian.stuedemann@web.de>
2018-11-18 13:06:13 +0100

    3. freiwillige HA

---
+++ src/ha3/Car.java
@@ -0,0 +1,51 @@
+package ha3;
+
+import java.awt.Color;
+
+import gridworld.framework.actor.Actor;
+import gridworld.framework.actor.Rock;
+import gridworld.framework.grid.Grid;
+import gridworld.framework.grid.Location;
+
+public class Car extends Actor {
+	int speed;
+	
+	Car (int speed) {
+		this.speed = speed;
+		setColor(Color.RED);
+		setDirection(180);
+	}
+	
+    public void move() {
+    	if (speed < 5) {
+    		speed++;
+    	}
+    	if (Math.random() < 0.2) {
+    		speed--;
+    	}
+        Grid<Actor> gr = getGrid();
+        Location loc = getLocation();
+        int row = loc.getRow();
+        int col = loc.getCol();
+    	for (int i = 1; i <= speed; ++i) {
+    		Location next = new Location (row, col + i);
+            if (gr.isValid(next)) {
+            	Actor neighbor = gr.get(next);
+            	if (neighbor instanceof Rock || neighbor instanceof Car) {
+            		speed = i - 1;
+            		break;
+            	}
+            }
+    	}
+        Location next = new Location (row, col + speed);       
+        if (gr.isValid(next))
+            moveTo(next);
+        else
+            removeSelfFromGrid();
+    }
+    
+    public void act() {
+    	move();
+    }
+
+}
