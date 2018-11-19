f21d29af78a8e631b215bf0190beed71fa1912c2
Pascal <Pascal@192.168.0.9>
2018-11-19 21:54:40 +0100

    Update Car, und MainCarWorld; Status: nicht fertig für Kontrolle

--- src/gridworld/carWorld/Car.java
+++ src/gridworld/carWorld/Car.java
@@ -1,16 +1,37 @@
 package gridworld.carWorld;
 
 import gridworld.framework.actor.Actor;
+import gridworld.framework.actor.Flower;
+import gridworld.framework.actor.Rock;
 import gridworld.framework.grid.Grid;
 import gridworld.framework.grid.Location;
 
 public class Car extends Actor {
-	int speed = 5;
+	int speed = 3;
 	
 	@Override
 	public void act() {
 		setDirection(90);
-		move();
+		for (int i = speed;i>0 ; i--) {
+			if (!checkRock()) {
+			move();	
+			}
+		}
+		accelerate();
+		dawdle();
+		
+	}
+	
+	public boolean checkRock() {
+		Grid<Actor> gr = getGrid();
+        if (gr == null) return false;
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        Actor neighbor = gr.get(next);
+		if (neighbor instanceof Rock) {
+			this.speed=0;
+			return true;
+		} else return false;
 	}
 	
 	public void move()
@@ -25,5 +46,32 @@ public class Car extends Actor {
         else
             removeSelfFromGrid();
     }
+	
+	public boolean canMove()
+    {
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+            return false;
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        if (!gr.isValid(next)) {
+        	removeSelfFromGrid();
+            return false;}
+        Actor neighbor = gr.get(next);
+        return (neighbor == null) || (neighbor instanceof Rock);
+    }
+	
+	public void accelerate() {
+		if (speed<5) {
+			speed++;
+		}
+	}
+	
+	public void dawdle() {
+		if ((Math.random()<=0.2) && (speed>0)) {
+			speed--;
+			System.out.println(speed);
+		}
+	}
 
 }
--- src/gridworld/carWorld/MainCarWorld.java
+++ src/gridworld/carWorld/MainCarWorld.java
@@ -3,6 +3,7 @@ package gridworld.carWorld;
 import com.sun.prism.paint.Color;
 
 import gridworld.framework.actor.ActorWorld;
+import gridworld.framework.actor.Rock;
 import gridworld.framework.grid.BoundedGrid;
 import gridworld.framework.grid.Location;
 import gridworld.framework.world.*;
@@ -11,14 +12,53 @@ public class MainCarWorld {
 	
 	public static void main (String args []) {
 		
-		BoundedGrid grid1 = new BoundedGrid(1,10);
+		BoundedGrid grid1 = new BoundedGrid(10,50);
 		ActorWorld world1 = new ActorWorld (grid1);
 		Car car1 = new Car();
 		Location loc1 = new Location(0,0);
 		car1.putSelfInGrid(grid1, loc1);
+		Rock rock1 = new Rock();
+		Location loc2 = new Location(0,4);
+		rock1.putSelfInGrid(grid1, loc2);
+		
+		Car car2 = new Car();
+		Location loc3 = new Location(1,0);
+		car2.putSelfInGrid(grid1, loc3);
+		
+		Car car3 = new Car();
+		Location loc4 = new Location(2,0);
+		car3.putSelfInGrid(grid1, loc4);
+		
+		Car car4 = new Car();
+		Location loc5 = new Location(3,0);
+		car4.putSelfInGrid(grid1, loc5);
+		
+		Car car5 = new Car();
+		Location loc6 = new Location(4,0);
+		car5.putSelfInGrid(grid1, loc6);
+		
+		Car car6 = new Car();
+		Location loc7 = new Location(5,0);
+		car6.putSelfInGrid(grid1, loc7);
+		
 		
 		world1.show();
 		
 	}
 
 }
+// ToDo:
+// Error bei Rausfahren lösen
+// CarSource erstellen
+
+
+
+//Fragen:
+
+// Wieso gibt es eine Klasse World und eine Klasse Actorworld?
+
+// Wieso dreht sich mein Auto-GIF?
+// Wieso wird das Bild des Auto komisch angezeigt?
+// Soll Auto den Stein iwann wegmachen?
+
+// Was bedeuten die Warnungen: The expression of type BoundedGrid needs unchecked conversion to conform to Grid<Actor>
