3976ae41e52e285471d7a296f23f749dcb3e52fe
Marc <dziakowski@vsp.tu-berlin.de>
2018-11-26 14:42:21 +0100

    rest uebung 6

---
+++ src/uebung6/Animal.java
@@ -0,0 +1,13 @@
+package uebung6;
+
+public class Animal {
+	
+	void onlyAnimal() {
+		System.out.println("Animal");
+	}
+	
+	void eat() {
+		System.out.println("Animal eating");
+	}
+	
+}
---
+++ src/uebung6/Dog.java
@@ -0,0 +1,13 @@
+package uebung6;
+
+public class Dog extends Animal {
+	
+	void bark() {
+		System.out.println("wuff");
+	}
+	
+	void eat() {
+		System.out.println("Dog is eating");
+	}
+
+}
