621e3b0f1c305723cf76dc030acdd5bb7f2bad65
u189526 <u189526@R-131.cadlab.tu-berlin.de>
2018-12-04 17:45:10 +0100

    8. Übung

---
+++ src/uebung8/PredatorMain.java
@@ -0,0 +1,18 @@
+package uebung8;
+
+import gridworld.framework.actor.ActorWorld;
+import uebung5.Breeder;
+
+class PredatorMain {
+
+	public static void main(String[] args) {
+		 ActorWorld world = new ActorWorld();
+		 Predator predator = new Predator();
+		 BabyBug babyBug = new BabyBug();
+		 world.add(predator);
+		 world.add(babyBug);
+		 world.add(new Breeder());
+		 world.show();
+	}
+
+}
