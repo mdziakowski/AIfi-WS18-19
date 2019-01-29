48798bc4689bf6b12f7c66e421ef6185939687a0
kainagel <nagel@vsp.tu-berlin.de>
2019-01-29 14:52:06 +0100

    add vl package

---
+++ src/vl/objectHierarchiesAbc/Main.java
@@ -0,0 +1,15 @@
+package vl.objectHierarchiesAbc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Main{
+
+	public static void main ( String [] args ){
+
+		List<Agent> agents = new ArrayList<>() ;
+
+
+	}
+
+}
